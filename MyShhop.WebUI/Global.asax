de3b685717c9d9bac17e0768cb469db9342dd378
dhruv patel <pateldhruv9509@gmail.com>
2022-02-22 17:14:30 -0500

    Initial Solution Setup

---
+++ MyShhop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShhop.WebUI.MvcApplication" Language="C#" %>
---
+++ MyShhop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShhop.WebUI.Startup))]
+namespace MyShhop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
